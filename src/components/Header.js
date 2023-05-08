1dcd6a6c2317c7d422f1ec4db220ea2ae77125be
Alexandr Zuev <zuevalex@yandex.ru>
2023-05-09 00:21:07 +0500

    начал css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import logo from "../images/logo.svg";
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Header() {
+function Header({title, route, email, onClick}) {
   return (
     <header className="header container">
       <img src={logo} alt="Логотип сайта" className="logo" />
+      <div className="header__auth">
+        <p className="header__email">e-mail@yandvbdfdfdvddfbdfex.ru</p>
+        {/* <Link to={route} className="header__auth-out" onClick={onClick}>{title}</Link> */}
+        <button className="header__out-link">Выйти</button>
+      </div>
     </header>
   );
 }
